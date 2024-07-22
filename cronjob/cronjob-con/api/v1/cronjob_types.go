f5d7ebe6bde063aa2a9e8b736c51f5401546ce14
srishti <srishti.dutta1111@gmail.com>
2024-07-22 12:01:55 +0530

    Added the code for the cronjob status

--- cronjob/cronjob-con/api/v1/cronjob_types.go
+++ cronjob/cronjob-con/api/v1/cronjob_types.go
@@ -87,6 +87,16 @@ type CronJobSpec struct {
 type CronJobStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// A list of pointers to currently running jobs.
+	// +optional
+	Active []corev1.ObjectReference `json:"active,omitempty"`
+
+	// Information when was the last time the job was successfully scheduled.
+	// +optional
+	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
 }
 
 //+kubebuilder:object:root=true
